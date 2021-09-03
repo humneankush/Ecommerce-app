4ebdd7bd8b54c677832d071d7f7bf3884b9ee653
humneankush <humneankush@gmail.com>
2021-09-03 21:28:50 +0530

    components and registration and login pages is completed and product page also completed

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,94 @@
+import React from "react";
+import styled from "styled-components";
+
+import { Search, ShoppingCartRounded } from "@material-ui/icons";
+import { Badge } from "@material-ui/core";
+
+const Container = styled.div`
+  height: 60px;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  padding: 10px 20px;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`;
+
+const Language = styled.span`
+  font-size: 14px;
+  cursor: pointer;
+`;
+
+const SearchContainer = styled.div`
+  border: 0.5px solid lightgray;
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+  padding: 5px;
+`;
+
+const Input = styled.input`
+  border: none;
+  .no-outline:focus {
+    outline: none;
+  }
+`;
+
+const Center = styled.div`
+  flex: 1;
+  text-align: center;
+`;
+
+const Logo = styled.h1`
+  font-weight: bold;
+`;
+
+const Right = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+`;
+
+const MenuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  margin-left: 25px;
+`;
+
+const Navbar = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+          <Language>EN</Language>
+          <SearchContainer>
+            <Input placeholder="search" />
+            <Search style={{ color: "gray", fontsize: 16 }} />
+          </SearchContainer>
+        </Left>
+        <Center>
+          <Logo>Ecom</Logo>
+        </Center>
+        <Right>
+          <MenuItem>REGISTER</MenuItem>
+          <MenuItem>SIGN IN</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={4} color="primary">
+              <ShoppingCartRounded />
+            </Badge>
+          </MenuItem>
+        </Right>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Navbar;
